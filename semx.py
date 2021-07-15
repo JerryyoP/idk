125b97fe1f2a4b87adf9a89fca902a818f8c2d12
JerryyoP <73817441+JerryyoP@users.noreply.github.com>
2021-07-16 03:22:06 +0530

    Update semx.py

--- semx.py
+++ semx.py
@@ -4,8 +4,8 @@ import pyrogram
     
 with pyrogram.Client(
     "pepe", 
-    api_id = 1273127,
-    api_hash = "2626aee4ea587947c6a703f1a0d6a3cc") as gensession:
+    api_id = 2622951,
+    api_hash = "52b92d6003747dc2d101cd8f5a081d38") as gensession:
     os.system('clear')
     message_ = f"#STRING_SESSION\n<code>{gensession.export_session_string()} </code>\n\n<b>>> Powered by ThePepeUserbot!</b>"
     gensession.send_message(
@@ -13,5 +13,7 @@ with pyrogram.Client(
         message_,
         parse_mode = "html"
     )
-    time.sleep(1) 
+    time.sleep(1)
+    print(message_)
     print("\nDone! Your string session has successfully been sent to your saved messages section vro ;)\n")
+    os.remove("pepe.session")
